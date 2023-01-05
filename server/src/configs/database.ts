00be9b9550b8843b49637ab10dbd87fe61ba210d
KiTiboD Phaphanwoe <101421091+300degree@users.noreply.github.com>
2023-01-05 15:29:24 +0000

    modified:   server/src/configs/database.ts 	modified:   server/src/server.ts

--- server/src/configs/database.ts
+++ server/src/configs/database.ts
@@ -4,16 +4,18 @@ export default class ConnDB {
 
     private username: string;
     private password: string;
+    private dataabase_name: string;
 
-    constructor(username: string, password: string) {
+    constructor(username: string, password: string, dataabase_name: string) {
         this.username = username;
         this.password = password;
+        this.dataabase_name = dataabase_name;
         this.mongoDB()
     }
 
     // Connection Database
     public mongoDB():void {
-        const Database = `mongodb+srv://${this.username}:${this.password}@cluster0.nogndxd.mongodb.net/member?retryWrites=true&w=majority`;
+        const Database = `mongodb+srv://${this.username}:${this.password}@cluster0.nogndxd.mongodb.net/${this.dataabase_name}?retryWrites=true&w=majority`;
         mongoose.Promise = global.Promise;
         mongoose.connect(Database)
         .then(() => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,9 @@ import Server from "./configs/app";
 import Network from "./configs/env/network";
 import ConnDB from "./configs/database";
 
-const connectDB = new ConnDB("admin", "passw0rd"); // input your "Username", "Password"
-const PORT = new Network();
-const server = new Server(PORT.port);
+const connectDB = new ConnDB("", "", ""); // input your "Username", "Password", "DatabaseNameCol"
+const config = new Network();
+const server = new Server(config.port);
 
 connectDB.mongoDB()
 server.run();
